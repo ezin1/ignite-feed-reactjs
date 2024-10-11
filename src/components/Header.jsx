1b17037c9736a3ffd28783a771997f7f70359764
ezin1 <93697880+ezin1@users.noreply.github.com>
2024-10-11 16:59:00 -0300

    feat: create global styles and initial header code and style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
 import styles from './Header.module.css';
-
+import igniteLogo from '../assets/ignite-logo.svg';
 export function Header(){
   return (
-    <strong className={styles.header}>Ignite Feed</strong>
+    <header className={styles.header}>
+      <strong>Ignite Feed</strong>
+    </header>
   )
 }

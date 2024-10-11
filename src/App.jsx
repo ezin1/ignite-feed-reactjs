9aacd682af68f32cbfc9a9d8395a2493d56a3643
ezin1 <93697880+ezin1@users.noreply.github.com>
2024-10-11 15:45:55 -0300

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Header } from './components/Header';
+import { Post } from './Post';  
+import './styles.css';
+export function App() {
+  return (
+    <div>
+      <Header/>
+      <Post 
+        author="Ézio" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam et recusandae magnam tempora debitis ducimus ut saepe dolore, omnis necessitatibus possimus numquam dolores eveniet harum nobis a accusamus, molestiae porro."
+      />
+      <Post 
+        author="Altair" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam et recusandae magnam tempora debitis ducimus ut saepe dolore, omnis necessitatibus possimus numquam dolores eveniet harum nobis a accusamus, molestiae porro."
+      />
+    </div>
+  )
+}
+
+
+
